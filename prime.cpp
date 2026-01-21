a2c165353393e872db9abe2131447c842a754265
deepak <kumarankit1731999@gmail.com>
2026-01-21 18:10:30 +0530

    prime number

--- prime.cpp
+++ prime.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-using namespace std ;
-int main(){
+using namespace std;
+int main()
+{
     int p;
-    cin>>p;
-    int i=2;
-    while(i<p){
-        if(p%i == 0){
-            cout<<"not a prime";
+    cin >> p;
+    int i = 2;
+    while (i < p)
+    {
+        if (p % i == 0)
+        {
+            cout << "not a prime";
             break;
         }
-       else{
-        cout<<"Prime Number";
-       }
-       i+=1;
+        else
+        {
+            cout << "Prime Number";
+            break;
+        }
+        i += 1;
     }
-}
+} 
+
+
+// break is used to stop iteration while
+// continue is used to skip the particualr 
+//iteration
