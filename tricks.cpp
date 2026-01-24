951fc12ecabb17667117b95acb3a8f08d565fe5d
deepak <kumarankit1731999@gmail.com>
2026-01-24 18:25:57 +0530

    deciaml

---
+++ tricks.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ( ){
+    // for to arrange the digit in original sequence
+    // {
+    //     ans=0;
+    //     ans=(ans*10)+ans;
+
+    // }
+
+    //for digit to be arrange  in a reverse sequence
+    // {
+    //     ans=0;
+    //     ans=(digit*10^i)+ans;
+    // }
+    
+}
